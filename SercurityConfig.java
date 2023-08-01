7c19d888e7f359d205212c5693be6b2f5fa012d7
zxcvcx123 <128358644+zxcvcx123@users.noreply.github.com>
2023-08-01 15:52:14 +0900

    Update SercurityConfig.java

--- SercurityConfig.java
+++ SercurityConfig.java
@@ -1,5 +1,22 @@
 /* 경로 프로젝트/src/main/java/com.example.demo/config */
 
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.context.DelegatingApplicationListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
+
+
 //시큐리티 핵심 기능들을 컨트롤 하는 클래스
 @Configuration
 @EnableWebSecurity
@@ -17,9 +34,9 @@ public class SercurityConfig {
 
 		 http
 		.authorizeHttpRequests()
-		.requestMatchers("/main/**").authenticated() //해당 url주소로 들어오면 인증이 필요해
-		.requestMatchers("/manager/**").access(new WebExpressionAuthorizationManager("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')"))//manager 쪽으로 들어올려면 어드민 or 매니저 권한이 있는 사람만 들어와 / hasAnyRole 은 여러권한
-		.requestMatchers("/admin/**").access(new WebExpressionAuthorizationManager("hasRole('ROLE_ADMIN')")) //admin은 어드민 권한 있는 사람만 들어와 / hasRole은 단일 권한
+		.requestMatchers(new AntPathRequestMatcher("/main/**")).authenticated() //해당 url주소로 들어오면 인증이 필요해
+		.requestMatchers(new AntPathRequestMatcher("/manager/**")).access(new WebExpressionAuthorizationManager("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')"))//manager 쪽으로 들어올려면 어드민 or 매니저 권한이 있는 사람만 들어와 / hasAnyRole 은 여러권한
+		.requestMatchers(new AntPathRequestMatcher("/admin/**")).access(new WebExpressionAuthorizationManager("hasRole('ROLE_ADMIN')")) //admin은 어드민 권한 있는 사람만 들어와 / hasRole은 단일 권한
 		//anyRequest()모든 리퀘스트에 대한 / permitAll() 하면 위에 따로 권한 설정한 페이지 외에는 전부 접속 가능 
 		//기본 로그인 페이지가 노출되지 않고 바로 접근이 가능 / authenticated() = 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
 		.anyRequest().permitAll()
